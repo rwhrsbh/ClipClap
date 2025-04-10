83d0914ea3e16adc54cc27478edf3585ac304bc6
Max <maksdndz1@gmail.com>
2025-04-10 23:47:22 +0300

    Initial Commit

---
+++ ClipClap/ClipClapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipClapApp.swift
+//  ClipClap
+//
+//  Created by mac on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipClapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipClapTests/ClipClapTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClipClapTests.swift
+//  ClipClapTests
+//
+//  Created by mac on 10.04.2025.
+//
+
+import Testing
+
+struct ClipClapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
